27ae932d22ee85379e4f271b391598c1e7d6438d
John Else <john.else@citrix.com>
2013-12-23 17:20:13 +0000

    Add to test_gntshr_gnttab
    Signed-off-by: John Else <john.else@citrix.com>

--- test_gntshr_gnttab/test_gntshr_gnttab.c
+++ test_gntshr_gnttab/test_gntshr_gnttab.c
@@ -1,48 +1,59 @@
+#include <asm-generic/mman-common.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include <xenctrl.h>
 
-void *share_pages(
-        xc_gntshr *gntshr_interface,
-        uint32_t domid,
-        int count,
-        uint32_t *refs) {
-    void *share;
-
-    share = xc_gntshr_share_pages(gntshr_interface, domid, count, refs, 0);
-
-    return share;
-}
-
 int main(int argc, char **argv) {
     xc_gntshr *gntshr_if;
     xc_gnttab *gnttab_if;
 
-    void* share;
+    uint32_t *refs;
+    void* local_share;
+    void* remote_share;
+
+    int err;
 
     gntshr_if = xc_gntshr_open(NULL, 0);
     if(!gntshr_if) {
         printf("Failed to open gntshr interface.\n");
-        goto Error;
+        exit(1);
     }
     gnttab_if = xc_gnttab_open(NULL, 0);
     if(!gnttab_if) {
         printf("Failed to open gnttab interface.\n");
-        goto Error;
+        exit(1);
+    }
+
+    refs = malloc(sizeof(uint32_t));
+    local_share = xc_gntshr_share_pages(gntshr_if, 0, 1, refs, 0);
+    if(!local_share) {
+        printf("Failed to share memory.\n");
+        exit(1);
+    }
+
+    remote_share = xc_gnttab_map_grant_ref(gnttab_if, 0, *refs, PROT_READ);
+    if(!remote_share) {
+        printf("Failed to map memory.\n");
+        exit(1);
+    }
+
+    err = xc_gnttab_munmap(gnttab_if, remote_share, 1);
+    if(err) {
+        printf("Failed to unmap memory - got error code %d.\n", err);
+    }
+
+    err = xc_gntshr_munmap(gntshr_if, local_share, 1);
+    if(err) {
+        printf("Failed to unshare memory - got error code %d.\n", err);
     }
 
     if(xc_gntshr_close(gntshr_if)) {
         printf("Failed to close gntshr interface.\n");
-        goto Error;
     }
     if(xc_gnttab_close(gnttab_if)) {
         printf("Failed to close gnttab interface.\n");
-        goto Error;
     }
 
     exit(0);
-
-Error:
-    exit(1);
 }
