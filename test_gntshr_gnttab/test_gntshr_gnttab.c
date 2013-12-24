fb98a7907a81c8784ee1022fd2fe08752fac81cf
John Else <john.else@citrix.com>
2013-12-24 16:30:26 +0000

    Refactor main test
    Signed-off-by: John Else <john.else@citrix.com>

--- test_gntshr_gnttab/test_gntshr_gnttab.c
+++ test_gntshr_gnttab/test_gntshr_gnttab.c
@@ -7,7 +7,7 @@
 #define PAGE_COUNT 1
 #define DOMID 0
 
-int main(int argc, char **argv) {
+int test_open_once() {
     xc_gntshr *gntshr_if;
     xc_gnttab *gnttab_if;
 
@@ -22,18 +22,18 @@ int main(int argc, char **argv) {
     gntshr_if = xc_gntshr_open(NULL, 0);
     if(!gntshr_if) {
         printf("Failed to open gntshr interface.\n");
-        exit(1);
+        return 1;
     }
     gnttab_if = xc_gnttab_open(NULL, 0);
     if(!gnttab_if) {
         printf("Failed to open gnttab interface.\n");
-        exit(1);
+        return 1;
     }
 
     local_share = xc_gntshr_share_pages(gntshr_if, DOMID, PAGE_COUNT, refs, 0);
     if(!local_share) {
         printf("Failed to share memory.\n");
-        exit(1);
+        return 1;
     }
 
     for(i = 0; i < PAGE_COUNT; i++) {
@@ -43,28 +43,48 @@ int main(int argc, char **argv) {
     for(i = 0; i < PAGE_COUNT; i++) {
         domids[i] = DOMID;
     }
-    remote_share = xc_gnttab_map_grant_refs(gnttab_if, PAGE_COUNT, domids, refs, PROT_READ);
+    remote_share = xc_gnttab_map_grant_refs(
+            gnttab_if,
+            PAGE_COUNT,
+            domids,
+            refs,
+            PROT_READ);
     if(!remote_share) {
         printf("Failed to map memory.\n");
-        exit(1);
+        return 1;
     }
 
     err = xc_gnttab_munmap(gnttab_if, remote_share, 1);
     if(err) {
         printf("Failed to unmap memory - got error code %d.\n", err);
+        return 1;
     }
 
     err = xc_gntshr_munmap(gntshr_if, local_share, 1);
     if(err) {
         printf("Failed to unshare memory - got error code %d.\n", err);
+        return 1;
     }
 
     if(xc_gntshr_close(gntshr_if)) {
         printf("Failed to close gntshr interface.\n");
+        return 1;
     }
     if(xc_gnttab_close(gnttab_if)) {
         printf("Failed to close gnttab interface.\n");
+        return 1;
     }
 
-    exit(0);
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    int err;
+
+    err = test_open_once();
+    if(err) {
+        return 1;
+    }
+
+    return 0;
 }
